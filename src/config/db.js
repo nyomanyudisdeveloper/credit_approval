889aceb8e06cdfe20ef0dccd93cc1ba289a3c8d3
nyomanyudisdeveloper <nyomanyudisdeveloper@gmail.com>
2024-11-22 17:18:38 +0700

    try

--- src/config/db.js
+++ src/config/db.js
@@ -7,11 +7,20 @@ const enviroment = process.env.ENV || 'development'
 var pool = null
 if(enviroment == 'development'){
     pool = new Pool({
-        host: process.env.PG_HOST,
-        port: process.env.PG_PORT,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: process.env.PG_DATABASE
+        host: process.env.PG_HOST_DEV,
+        port: process.env.PG_PORT_DEV,
+        user: process.env.PG_USER_DEV,
+        password: process.env.PG_PASSWORD_DEV,
+        database: process.env.PG_DATABASE_DEV
+    })
+}
+else if(enviroment == 'local'){
+    pool = new Pool({
+        host: process.env.PG_HOST_LOCAL,
+        port: process.env.PG_PORT_LOCAL,
+        user: process.env.PG_USER_LOCAL,
+        password: process.env.PG_PASSWORD_LOCAL,
+        database: process.env.PG_DATABASE_LOCAL
     })
 }
 
