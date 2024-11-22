044b614ece02e6c75cce7cf84fd0f7d2ced969bc
nyomanyudisdeveloper <nyomanyudisdeveloper@gmail.com>
2024-11-22 17:13:21 +0700

    try 2

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,8 @@ const {Pool} = pkg;
 import dotenv from 'dotenv'
 dotenv.config()
 
+const enviroment = process.env.NODE_ENV || 'development'
+
 const pool = new Pool({
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
